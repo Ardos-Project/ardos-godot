216866ac77e668d75972ed48512f3b21dac3fa9a
Kylie Smith <kylie.smithy01@gmail.com>
2024-02-14 16:22:15 +1000

    Push

--- demo/DemoServerUD.gd
+++ demo/DemoServerUD.gd
@@ -1,1 +1,10 @@
 class_name DemoServerUD extends ServerRepository
+
+
+func _init(
+	dc_file_names: PackedStringArray = [],
+	dc_suffix: String = "",
+	min_channel: int = 0,
+	max_channel: int = 0
+):
+	super._init(dc_file_names, dc_suffix, min_channel, max_channel)
--- demo/demo.gd
+++ demo/demo.gd
@@ -1,19 +1,56 @@
 extends Node3D
 
-var _client: bool = true
+const DC_FILES: PackedStringArray = ["data/dclass/demo.dc"]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if self._client:
-		var _cr = DemoClient.new(["data/dclass/demo.dc"])
+	if DisplayServer.get_name() == "headless":
+		# We're running as a headless server.
+
+		# Fetch the base channel we've been allocated.
+		var base_channel: int = int(self._get_cmdline_arg("air-base-channel", "400000000"))
+		# Fetch the number of channels we've been allocated.
+		var channel_alloc: int = int(self._get_cmdline_arg("air-channel-allocation", "1000000"))
+		# Fetch the State Server we've been allocated.
+		var stateserver_id: int = int(self._get_cmdline_arg("air-channel-allocation", "1001"))
+		# Fetch the Message Director host address.
+		var md_host: String = self._get_cmdline_arg("air-connect", "127.0.0.1")
+
+		if "--ud" in OS.get_cmdline_user_args():
+			var _cls = load("res://demo/DemoServerUD.gd")
+
+			var _ud = _cls.new(DC_FILES, "UD", base_channel, base_channel + channel_alloc)
+			_ud.name = "DemoServerUD"
+			add_child(_ud)
+
+			_ud.connect_to_server(md_host, 7100)
+
+		elif "--ai" in OS.get_cmdline_user_args():
+			var _cls = load("res://demo/DemoServerAI.gd")
+
+			var _ai = _cls.new(DC_FILES, "AI", base_channel, base_channel + channel_alloc)
+			_ai.name = "DemoServerAI"
+			add_child(_ai)
+
+			_ai.connect_to_server(md_host, 7100)
+	else:
+		# We're running as a client.
+		var _cls = load("res://demo/DemoClient.gd")
+
+		var _cr = _cls.new(DC_FILES)
 		_cr.name = "ClientRepository"
 		add_child(_cr)
 
 		_cr.connect_to_server("127.0.0.1", 6667, "godot-demo")
-	else:
-		var _ud = DemoServerUD.new(["data/dclass/demo.dc"])
-		_ud.name = "DemoServerUD"
-		add_child(_ud)
 
-		_ud.connect_to_server("127.0.0.1", 7100)
+
+func _get_cmdline_arg(arg_name: String, default: String = "") -> String:
+	# Fetch the value from the cmd line.
+	var indx: int = OS.get_cmdline_user_args().find(arg_name)
+	if indx != -1:
+		return OS.get_cmdline_user_args()[indx + 1]
+
+	# If it doesn't exist, try get it as an environment variable.
+	var env_var: String = OS.get_environment(arg_name)
+	return env_var if env_var != "" else default
