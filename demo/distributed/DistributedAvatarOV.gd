415f39e69e348600c0edf9d98956f6a5e70c5ea9
Kylie Smith <kylie.smithy01@gmail.com>
2024-02-24 15:40:00 +1000

    Push

--- demo/distributed/DistributedAvatarOV.gd
+++ demo/distributed/DistributedAvatarOV.gd
@@ -1,1 +1,14 @@
 class_name DistributedAvatarOV extends DistributedAvatar
+
+@onready var _pickNameContainer = get_node("/root/Root/UI/PickNameContainer")
+
+
+func _ready():
+	super._ready()
+
+	# Reparent the local camera.
+	get_tree().get_first_node_in_group("local_camera").reparent(_avatar_model)
+
+	# We could do this in AuthMgrUD acceptLogin() function,
+	# but we just do that here.
+	_pickNameContainer.visible = false
