31408ad253d5edabd0839c51da4f510073ac7342
Kylie Smith <kylie.smithy01@gmail.com>
2024-02-27 18:41:29 +1000

    Push

--- demo/distributed/DistributedDistrict.gd
+++ demo/distributed/DistributedDistrict.gd
@@ -1,1 +1,26 @@
 class_name DistributedDistrict extends DistributedObject
+
+static var DISTRICTS: Array = []
+
+var _name: String = ""
+var _available: bool = false
+
+
+func announce_generate():
+	super.announce_generate()
+
+	DISTRICTS.append(self)
+
+
+func delete():
+	super.delete()
+
+	DISTRICTS.erase(self)
+
+
+func set_district_name(district_name: String) -> void:
+	self._name = district_name
+
+
+func set_available(available: bool) -> void:
+	self._available = available
